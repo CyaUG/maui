978d829b5c6109b4a7f58f564e6291ee9c988778
wagaana <wagaanaa@gmail.com>
2025-01-12 15:26:02 +0300

    first commit

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Youth.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Youth.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Youth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6a09226277efe3f986214857f2ef925f81793f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+69dce56810562534c2434f19af8e8e0c4ce77e25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Youth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Youth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
