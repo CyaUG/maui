bf4a7cf2f0df20a77a7805587e6daa30a656fab3
wagaana <wagaanaa@gmail.com>
2025-01-13 16:50:37 +0300

    first commit

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Youth.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Youth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Youth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6bd1bace0b66e39e84e443f118c27c426ab9879c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e3a10767de25fa04354d7bace566aa69c59c62f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Youth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Youth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
